14c092ff58e9b946d9605cf68b7a6ea88ac0fa30
AliMyr <rabo41ali@gmail.com>
2025-04-04 20:51:33 +0500

    (TEST) Jump Mechanics

--- Assets/Ali/Scripts/Mechanic/Implementation/JumpMechanic.cs
+++ Assets/Ali/Scripts/Mechanic/Implementation/JumpMechanic.cs
@@ -1,13 +1,41 @@
-public class JumpMechanic : IMechanic
+using UnityEngine;
+
+public class JumpMechanic : IMechanic, IUpdatableMechanic
 {
     private Character character;
-    public void Initialize(Character character) => this.character = character;
+    private float jumpForce = 5f;
+    private float gravity = -9.81f;
+    private float verticalVelocity;
+
+    public void Initialize(Character character)
+    {
+        this.character = character;
+        verticalVelocity = 0f;
+    }
+
     public void Enable()
     {
-        // Подписаться на ввод для прыжка, добавить реализацию прыжка.
+        // Механика прыжка включена
     }
+
     public void Disable()
     {
-        // Отписаться от прыжкового ввода.
+        // Механика прыжка отключена
+    }
+
+    public void Update()
+    {
+        if (character.CharacterController.isGrounded)
+        {
+            verticalVelocity = -1f;
+            if (Input.GetButtonDown("Jump"))
+                verticalVelocity = jumpForce;
+        }
+        else
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+        Vector3 jumpMovement = new Vector3(0, verticalVelocity, 0);
+        character.CharacterController.Move(jumpMovement * Time.deltaTime);
     }
 }

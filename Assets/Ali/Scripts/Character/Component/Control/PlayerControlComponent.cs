14078edcf13443bd16106041edd8fc1e18686558
AliMyr <rabo41ali@gmail.com>
2025-04-07 22:34:46 +0500

    (TEST) Control Component

---
+++ Assets/Ali/Scripts/Character/Component/Control/PlayerControlComponent.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerControlComponent : IControlComponent
+{
+    private Character character;
+    private IInputService inputService;
+
+    private IMovementComponent MovementComponent => character.MovementComponent;
+    private IRotationComponent RotationComponent => character.RotationComponent;
+    private IInventoryComponent InventoryComponent => character.InventoryComponent;
+    private IJumpComponent JumpComponent => character.JumpComponent;
+
+    public void Initialize(Character character)
+    {
+        this.character = character;
+        inputService = GameManager.Instance.InputService;
+    }
+
+    public void OnUpdate()
+    {
+        float x = inputService.Direction.x;
+        float y = inputService.Direction.y;
+
+        Vector3 moveDirection = new Vector3(x, 0, z).normalized;
+        MovementComponent.Move(moveDirection);
+
+        RotationComponent.Rotate(moveDirection);
+
+        JumpComponent.Update();
+        InventoryComponent.Update();
+        
+    }
+
+
+}

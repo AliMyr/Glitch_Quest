77367975521b8f3e00fe28f6dbac49d760dbced5
AliMyr <rabo41ali@gmail.com>
2025-04-08 15:34:41 +0500

    (FIX) Rotate Player Component

--- Assets/Ali/Scripts/Character/Component/Control/PlayerControlComponent.cs
+++ Assets/Ali/Scripts/Character/Component/Control/PlayerControlComponent.cs
@@ -18,14 +18,23 @@ public class PlayerControlComponent : IControlComponent
 
     public void OnUpdate()
     {
-        float x = inputService.Direction.x;
-        float z = inputService.Direction.y;
-        Vector3 moveDirection = new Vector3(x, 0, z).normalized;
+        Vector2 inputDir = inputService.Direction;
+        Camera cam = Camera.main;
+        Vector3 camForward = cam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+        Vector3 camRight = cam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+        Vector3 moveDirection = (camRight * inputDir.x + camForward * inputDir.y).normalized;
+
         Vector3 horizontalMovement = MovementComponent.CalculateMovement(moveDirection);
         Vector3 verticalMovement = JumpComponent.CalculateJumpMovement(inputService.Jump);
         Vector3 totalMovement = (horizontalMovement + verticalMovement) * Time.deltaTime;
         character.CharacterController.Move(totalMovement);
-        RotationComponent.Rotate(moveDirection);
+
+        if (moveDirection != Vector3.zero)
+            RotationComponent.Rotate(moveDirection);
 
         if (inputService.Use)
             InventoryComponent.PickupItem();
